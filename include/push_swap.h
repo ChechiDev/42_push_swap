3535eb514bedd42442b30d423d66ef80f6bb1bb6
ChechiDev <sperez-l@cbr1s6.42barcelona.com>
2026-02-18 16:09:22 +0100

    push_swap.h Add: + typedef enum e_flag_opt + typedef struct s_stacks + funct: clear stash

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,13 +6,41 @@
 /*   By: sperez-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 13:29:01 by sperez-l          #+#    #+#             */
-/*   Updated: 2026/02/17 13:30:37 by sperez-l         ###   ########.fr       */
+/*   Updated: 2026/02/18 16:00:13 by sperez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* Data structure */
+
+typedef enum e_flag_opt
+{
+	SIMPLE,
+	MEDIUM,
+	COMPLEX,
+	ADAPTIVE,
+	BENCH
+}	t_flagtype;
+
+typedef struct s_stacks
+{
+	t_list	**stack_a;
+	t_list	**stack_b;
+	int		start;
+	int		end;
+}	t_stacks;
+
+/* Args Parse */
+
+
+/* Functions */
+void	clear_stash(char **stash);
 
 
 #endif 
