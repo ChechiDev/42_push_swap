f2f6f1d96e522a9520318761009244d36a4959fd
ChechiDev <sperez-l@cbr1s6.42barcelona.com>
2026-02-18 17:54:32 +0100

    Libft: Add: + ft_strncmp();

---
+++ libft/include/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sperez-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 17:47:35 by sperez-l          #+#    #+#             */
+/*   Updated: 2026/02/18 17:53:50 by sperez-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <ctype.h>
+
+/* Libft functions */
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+
+#endif
