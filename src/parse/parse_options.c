3cd1da23f7c617813968a2d307e8308306397071
Javier Sánchez de Arévalo López <Javier2@iMac-de-Javier.local>
2026-02-20 17:38:34 +0100

    cambios en parse_options y nuevo parse_num

--- src/parse/parse_options.c
+++ src/parse/parse_options.c
@@ -12,17 +12,43 @@
 
 #include "../include/push_swap.h"
 
-t_flagtype	switch_option(char	*option)
+t_opttype	switch_option(char	*opt_str)
 {
-	if ((ft_strcmp(option, "bench")) == 0)
+	if ((ft_strcmp(opt_str, "bench")) == 0)
 		return (BENCH);
-	else if ((ft_strcmp(option, "simple")) == 0)
+	else if ((ft_strcmp(opt_str, "simple")) == 0)
 		return (SIMPLE);
-	else if ((ft_strcmp(option, "medium")) == 0)
+	else if ((ft_strcmp(opt_str, "medium")) == 0)
 		return (MEDIUM);
-	else if ((ft_strcmp(optio, "complex")) == 0)
+	else if ((ft_strcmp(opt_str, "complex")) == 0)
 		return (COMPLEX);
-	esle if ((ft_strcmp ("adaptative")) == 0)
+	esle if ((ft_strcmp (opt_str, "adaptative")) == 0)
 		return (ADAPTATIVE);
 	return (UNDEFINED);
+}
+
+
+int	parse_options(int ac, char **av, t_opttype **options)
+{
+	t_opttype	alg;
+	int	bench;
+	int	error;
+
+	alg = UNDEFINED;
+	bench = 0;
+	*options = malloc(sizeof(t_opttype);
+	if (!*options)
+		return (0);
+	(*options)->count = 0;
+	(*options)->options = malloc(2 * sizeof(t_opttype));
+	if (!(*options)->options)
+		return (free(*options), 0);
+	>>>ERROR FUNCION<<<(ac, av, bench, alg); // nombre de función a definir
+	if (error)
+		return (error);
+	if (bench == 1)
+		(*options)->options[(*options)->count++] = BENCH;
+	if (alg != UNDEFINED)
+		(*options)->options[(*options)->count++] = alg;
+	return ((*options)->count);
 }
