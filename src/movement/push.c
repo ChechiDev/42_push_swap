2397e3e324d68e815c744eeb973c2a2e406c36ba
Javier Sánchez de Arévalo López <Javier2@iMac-de-Javier.local>
2026-02-24 17:06:07 +0100

    push

---
+++ src/movement/push.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+#include "push_swap.h"
+
+void	pb(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*tmp;
+
+	if (!*stack_a || !stack_a)
+		return ;
+	tmp = *stack_a;
+	*stack_a = tmp->next;
+	tmp->next = *stack_b;
+	*stack_b = tmp;
+	write (1, "pb\n", 3);
+}
+
+void	pa(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*tmp;
+
+	if (!*stack_b || !stack_b)
+		return ;
+	tmp = *stack_b;
+	*stack_b = tmp->next;
+	tmp->next = *stack_a;
+	*stack_a = tmp;
+	write (1, "pa\n", 3);
+}
