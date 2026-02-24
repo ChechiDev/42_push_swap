c6adbf6ff3f54a3fc3727ad31b05ce9d64552953
ChechiDev <sperez-l@cbr9s3.42barcelona.com>
2026-02-24 11:59:56 +0100

    check_first.c: + Comprueba los args introducidos por terminal

--- src/check/check_first.c
+++ src/check/check_first.c
@@ -6,23 +6,20 @@
 /*   By: sperez-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 17:18:47 by sperez-l          #+#    #+#             */
-/*   Updated: 2026/02/23 17:34:18 by sperez-l         ###   ########.fr       */
+/*   Updated: 2026/02/24 11:56:20 by sperez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	check_first(int argc, char argv, t_stacks stack)
+int	check_first(int argc, char **argv, t_stacks stack)
 {
 	if (argc == 1)
-	{
-		write(1, "Error\n", 5);
 		return (0);
-	}
 	else if (argc == 2)
 	{
-		if (!parse_options())
-		{
-		}
+		if (!parse_options(argc, argv, stack.stack_a))
+			return (0);
 	}
+	return (1);
 }
