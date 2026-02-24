8be9bd59599481a3de5ee2ecff407af95f1c32f2
Javier Sánchez de Arévalo López <Javier2@iMac-de-Javier.local>
2026-02-24 17:29:44 +0100

    rotate, unfinished

---
+++ src/movement/rotate.c
@@ -0,0 +1,38 @@
+#include "push_swap.h"
+
+void	ra(t_list **stack_a)
+{
+	t_list	*first;
+	t_list	*last;
+
+	if (!*stack_a || !stack_a)
+		return ;
+	first = *stack_a;
+	*stack_a = first->next;
+	first->next = NULL;
+	last = ???; /// función?
+	last->next = first;
+	write (1, "ra\n", 3);
+}
+
+void	rb(t_list **stack_b)
+{
+	t_list	*first;
+	t_list	*last;
+
+	if (!*stack_b || !stack_b)
+		return ;
+	first = *stack_b;
+	*stack_b = first->next;
+	first->next = NULL;
+	last = ??? /// función?
+	last->next = first;
+	write (1, "rb\n", 3);
+}
+
+void	rr(t_list **stack_a, t_list **stack_b)
+{
+	ra(stack_a);
+	rb(stack_b);
+	write (1, "rr\n", 3);
+}
