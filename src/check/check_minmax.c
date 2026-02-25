a7942c07266ff668b728a50da9cca37898fb28ca
ChechiDev <sperez-l@cbr11s4.42barcelona.com>
2026-02-25 19:06:02 +0100

    actualizo

--- src/check/check_minmax.c
+++ src/check/check_minmax.c
@@ -6,7 +6,7 @@
 /*   By: sperez-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 18:04:40 by sperez-l          #+#    #+#             */
-/*   Updated: 2026/02/24 13:33:08 by sperez-l         ###   ########.fr       */
+/*   Updated: 2026/02/25 18:54:40 by sperez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,32 +28,3 @@ int	check_minmax(char **argv)
 	}
 	return (1);
 }
-/*
-int	main(int argc, char **argv)
-{
-	int		arg_count;
-	char	**input_values;
-	int		result;
-	int		debug_result;
-
-	arg_count = argc;
-	input_values = argv + 1;
-
-	if (arg_count < 2)
-	{
-		printf("Usage: %s <num1> <num2> ...\n", argv[0]);
-		return (1);
-	}
-
-	result = check_minmax(input_values);
-	debug_result = result;
-
-	printf("Result: %d\n", debug_result);
-	if (debug_result == 1)
-		printf("All values inside INT range\n");
-	else
-		printf("Value outside INT range detected\n");
-
-	return (0);
-}
-*/
