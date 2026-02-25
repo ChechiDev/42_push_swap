e6938fbf0dac2a3f4c654359f22be1244354b171
Javier Sánchez de Arévalo López <Javier2@iMac-de-Javier.local>
2026-02-25 14:54:12 +0100

    reverse_rotate + Makefile + header

--- include/push_swap.h
+++ include/push_swap.h
@@ -113,5 +113,8 @@ void		rr(t_list **stack_a, t_list **stack_b);
 void		sa(t_list **stack_a);
 void		sb(t_list **stack_b);
 void		ss(t_list **stack_a, t_list **stack_b);
+void		rra(t_list **stack_a);
+void		rrb(t_list **stack_b);
+void		rrr(t_list **stack_a, t_list **stack_b);
 
 #endif
