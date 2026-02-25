eeb8db6aa546733d3865aac952cbfa4a3db4049c
Javier Sánchez de Arévalo López <Javier2@iMac-de-Javier.local>
2026-02-25 13:07:59 +0100

    Makefile y header con swap

--- include/push_swap.h
+++ include/push_swap.h
@@ -110,6 +110,8 @@ void		pa(t_list **stack_a, t_list **stack_b);
 void		ra(t_list **stack_a);
 void		rb(t_list **stack_b);
 void		rr(t_list **stack_a, t_list **stack_b);
+void		sa(t_list **stack_a);
+void		sb(t_list **stack_b);
+void		ss(t_list **stack_a, t_list **stack_b);
 
-
-#endif 
+#endif
